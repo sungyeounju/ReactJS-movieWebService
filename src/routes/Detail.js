be5d9f50b8b8bfcfb8ec597bec30cb4091c9f3e9
sungyeounju <yj0606yj@naver.com>
2023-05-05 01:17:53 +0900

    update : Router, Parameters

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+
 function Detail(){
- return (
+  const {id} = useParams();
+  const getMoive = async() => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json(); 
+    console.log(json)
+  };
+  useEffect(()=>{
+    getMoive();
+  },[])
+ return (    
     <h1>detail</h1>
  )
 }
